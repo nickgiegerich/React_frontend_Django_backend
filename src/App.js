f09a1697bcd746e6fffbfdf01d4a96f87857ea03
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2020-04-06 21:18:51 -0700

    frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,153 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component { 
+  constructor(props) { 
+    super(props);
+      this.state = {
+        todoList: [],
+        activeItem:{
+           id:null,
+           title:'',
+           completed:false,
+        },
+        editing:false,
+      }
+      this.fetchTasks = this.fetchTasks.bind(this)
+      this.handleChange = this.handleChange.bind(this)
+      this.handleSubmit = this.handleSubmit.bind(this)
+      this.getCookie = this.getCookie.bind(this)
+  };
+
+  getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
+
+  componentDidMount() { 
+    this.fetchTasks()
+  }
+
+  fetchTasks() { 
+    console.log('Fetching....')
+
+    fetch('http://127.0.0.1:8000/api/task-list/')
+    .then(response => response.json())
+    .then(data =>
+        this.setState({
+          todoList:data
+        })
+      )
+  }
+
+  handleChange(e) { 
+    var name = e.target.name
+    var value = e.target.value
+
+    console.log('Name:', name)
+    console.log('Value:', value)
+
+    this.setState({
+      activeItem:{
+        ...this.state.activeItem,
+        title:value
+      }
+    })
+  }
+
+  handleSubmit(e) { 
+    e.preventDefault()
+    console.log('ITEM:', this.state.activeItem)
+    
+    var csrftoken = this.getCookie('csrftoken')
+
+    var url = 'http://127.0.0.1:8000/api/task-create/'
+    fetch(url, {
+      method:'POST',
+      headers:{
+        'Content-type':'application/json',
+        'X-CSRFToken':csrftoken,
+      },
+      body:JSON.stringify(this.state.activeItem)
+    }).then((response) => {
+      this.fetchTasks()
+      this.setState({
+          activeItem:{
+          id:null,
+          title:'',
+          completed:false,
+          }
+      })
+    }).catch(function(error) {
+      console.log('ERROR:',error)
+    })
+  }
+
+
+  render() { 
+    
+    var tasks = this.state.todoList
+
+
+    return(
+        <div className="container">
+          <div id="task-container">
+
+            <div id="form-wrapper"> 
+              <form onSubmit={this.handleSubmit} id="form"> 
+                <div className="flex-wrapper"> 
+
+                  <div style={{flex: 6}}>
+                    <input onChange={this.handleChange} className="form-control" id="title" value={this.state.activeItem.title} type="text" name="title" placeholder="Enter..."></input>
+                  </div>
+
+                  <div style={{flex: 1}}>
+                    <input className="btn btn-warning" id="submit" type="submit" name="Add"></input>
+                  </div>
+
+                </div>
+              </form>
+            </div>
+
+            <div id="list-wrapper">
+                {tasks.map(function(task, index){
+                  return(
+                    <div key={index} className="task-wrapper flex-wrapper">
+
+                      <div style={{flex: 7}}>
+                        <span>{task.title}</span>
+                      </div>
+
+                      <div style={{flex: 1}}>
+                        <button className="btn btn-sm btn-outline-info">Edit</button>
+                      </div>
+
+                      <div style={{flex: 1}}>
+                        <button className="btn btn-sm btn-outline-dark delete">-</button>
+                      </div>
+
+                    </div>
+                  )
+                })}
+            </div>
+
+          </div>
+        </div>
+    )
+  }
+}
+
 
 export default App;
